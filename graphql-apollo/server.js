5cc36959a501aefc59c419ac1d6f24a8a1f52249
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-08-17 22:01:30 -0600

    Setup Graphiql and graphql

--- graphql-apollo/server.js
+++ graphql-apollo/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
-
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import bodyParser from 'body-parser';
 const server = express();
 
 console.log('changing the file!');
 
 server.get('/', (req, res) => {
     res.send('<html> <body> <h1> Hello world </h1> </body> </html>')
-})
+});
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({}));
+
+server.use('/graphiql', graphiqlExpress({
+    endpointURL: "/graphql"
+}));
 server.listen(4001, () => {
     console.log('listening on port 4001');
 });
